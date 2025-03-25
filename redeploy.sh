03bf259027117c2c175cdb73121aa0cc31744e3e
Thomas Bexton <98856985+thomasbexton@users.noreply.github.com>
2025-03-25 00:45:45 -0400

    Add GitHub page redeployment script

---
+++ redeploy.sh
@@ -0,0 +1,5 @@
+git checkout gh-pages && git merge main --no-edit
+npm run build
+git add dist -f && git commit -m "Deployment commit"
+git subtree push --prefix dist origin gh-pages
+git checkout main
